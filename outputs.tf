b3f94f021c120aca9f4aa03788edb774cb1b350a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "management_groups" {
-  description = "All management_group resources"
-  value       = azurerm_management_group.management_groups
-}
 output "management_groups_display_name" {
-  description = "List of display_name values across all management_groups"
-  value       = [for k, v in azurerm_management_group.management_groups : v.display_name]
+  description = "Map of display_name values across all management_groups, keyed the same as var.management_groups"
+  value       = { for k, v in azurerm_management_group.management_groups : k => v.display_name }
 }
 output "management_groups_name" {
-  description = "List of name values across all management_groups"
-  value       = [for k, v in azurerm_management_group.management_groups : v.name]
+  description = "Map of name values across all management_groups, keyed the same as var.management_groups"
+  value       = { for k, v in azurerm_management_group.management_groups : k => v.name }
 }
 output "management_groups_parent_management_group_id" {
-  description = "List of parent_management_group_id values across all management_groups"
-  value       = [for k, v in azurerm_management_group.management_groups : v.parent_management_group_id]
+  description = "Map of parent_management_group_id values across all management_groups, keyed the same as var.management_groups"
+  value       = { for k, v in azurerm_management_group.management_groups : k => v.parent_management_group_id }
 }
 output "management_groups_subscription_ids" {
-  description = "List of subscription_ids values across all management_groups"
-  value       = [for k, v in azurerm_management_group.management_groups : v.subscription_ids]
+  description = "Map of subscription_ids values across all management_groups, keyed the same as var.management_groups"
+  value       = { for k, v in azurerm_management_group.management_groups : k => v.subscription_ids }
 }
 output "management_groups_tenant_scoped_id" {
-  description = "List of tenant_scoped_id values across all management_groups"
-  value       = [for k, v in azurerm_management_group.management_groups : v.tenant_scoped_id]
+  description = "Map of tenant_scoped_id values across all management_groups, keyed the same as var.management_groups"
+  value       = { for k, v in azurerm_management_group.management_groups : k => v.tenant_scoped_id }
 }
 
